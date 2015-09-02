7e11ea87a07c2dbc8df7cd361e683b66c1775221
Avinash-barola <avinash.barola@github.com>
2015-09-02 15:14:05 +0530

    Update PostOrder.java

--- DS/DS3/PostOrder.java
+++ DS/DS3/PostOrder.java
@@ -2,59 +2,33 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 //class to create a binary search tree and its operation
 class Postorder {
-	Object nodeInformation;
-	Postorder rightChildAddress,leftChildAddress;
-	static Postorder rootNode;
-	Postorder() {
-		rootNode=null;
-	}	
-	//to insert a new node in tree
-	public Postorder insertNode(Postorder node,Object nodeInformation) {	
-		if(node==null) {     //base condition of recursive method
-			node=new Postorder();
-			node.nodeInformation=nodeInformation;
-			node.leftChildAddress=null;
-			node.rightChildAddress=null;
-		}
-		else if((int)node.nodeInformation>=(int)nodeInformation) {
-			node.leftChildAddress=insertNode(node.leftChildAddress,nodeInformation);
-		}
-		else {
-			node.rightChildAddress=insertNode(node.rightChildAddress, nodeInformation);
-		}
-		return node;
-	}			
-	//to show postorder traversal
-	public void displayPostorderTraversal(Postorder node) {
+	String result="";	
+	//to show post order traversal*/
+	public String getPostorderTraversal(Node node) {
 		if(node!=null) {
-			displayPostorderTraversal(node.leftChildAddress);
-			displayPostorderTraversal(node.rightChildAddress);
-			System.out.print(node.nodeInformation+"->");
+			getPostorderTraversal(node.leftChildAddress);                                                                                   
+			getPostorderTraversal(node.rightChildAddress);
+			result=result+node.nodeInformation+"->";
 		}	
+		return result;
 	}
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
-		Postorder PostorderObject=new Postorder();
-		System.out.println("Enter Choise");
-		try {
-			while(true) {
-				System.out.println("\n1:Insert a node\n2:Postorder display\n3:exit");
-				int choise=sc.nextInt();
-				switch(choise) {
-				case 1:
-					System.out.println("Enter value");
-					int nodeInformation=sc.nextInt();
-					rootNode=PostorderObject.insertNode(rootNode,nodeInformation);
-					break;
-				case 2:
-					PostorderObject.displayPostorderTraversal(rootNode);
-					break;
-				case 3:
-					System.exit(0);
-					break;
-				default:System.out.println("Enter correct choise");
-				}
-			}
-		}catch(InputMismatchException exception) {System.out.println("Please Enter only screen values");}
+		Node nodeObject=new Node();
+		Preorder preorderObject=new Preorder();
+		Postorder postorderObject=new Postorder();
+		//try {
+				nodeObject.rootNode=preorderObject.insertNode(nodeObject.rootNode,8);
+				nodeObject.rootNode=preorderObject.insertNode(nodeObject.rootNode,9);
+				nodeObject.rootNode=preorderObject.insertNode(nodeObject.rootNode,3);
+				nodeObject.rootNode=preorderObject.insertNode(nodeObject.rootNode,1);
+				nodeObject.rootNode=preorderObject.insertNode(nodeObject.rootNode,10);
+				nodeObject.rootNode=preorderObject.insertNode(nodeObject.rootNode,5);
+				nodeObject.rootNode=preorderObject.insertNode(nodeObject.rootNode,4);
+				nodeObject.rootNode=preorderObject.insertNode(nodeObject.rootNode,0);
+	
+				String result=postorderObject.getPostorderTraversal(nodeObject.rootNode);
+				System.out.println("Postorder Traversal is :"+result);
+		//}//catch(Exception exception) {System.out.println("Please Enter only Integer values");}
 	}
 }
