23728d42fbc84cc013e20ff584973d34e09827bd
Avinash-barola <avinash.barola@github.com>
2015-09-02 18:23:16 +0530

    Update InfixToPostfix.java

--- DS/DS2/InfixToPostfix.java
+++ DS/DS2/InfixToPostfix.java
@@ -1,72 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
-class Stack {
-	Object listInformation;
-	char listInformationInChar;
-	Stack addressOfNextNode,top;
-	public Stack() {
-		top=null;
-	}
-	public void pushAnItem(Object listInformation) {
-		Stack node=new Stack();
-		node.listInformation=listInformation;
-		if(top==null) {
-			top=node;
-			node.addressOfNextNode=null;
-		}
-		else {
-			node.addressOfNextNode=top;
-			top=node;
-		}
-	}
-	public void pushAnItem(char listInformationInChar) {
-		Stack node=new Stack();
-		node.listInformationInChar=listInformationInChar;
-		if(top==null) {
-			top=node;
-			node.addressOfNextNode=null;
-		}
-		else {
-			node.addressOfNextNode=top;
-			top=node;
-		}
-	}
-	public Stack popAnItem() {
-		Stack stackTopElement;
-		if(top==null) {
-			stackTopElement=null;
-			return stackTopElement;
-		}
-		else if(top.addressOfNextNode==null) {
-			stackTopElement=top;
-			top=null;
-			return stackTopElement;
-		}
-		else {
-			stackTopElement=top;
-			top=top.addressOfNextNode;
-			return stackTopElement;
-		}
-	}
-	public void display() {
-		if(top==null) {
-			System.out.println("Stack is empty");
-		}
-		else {
-			Stack traversingNode=top;
-			while(traversingNode!=null) {
-				System.out.println(traversingNode.listInformation);
-				traversingNode=traversingNode.addressOfNextNode;
-			}
-		}
-	}
-}
+
 class Precedence {
-	public int getPrcedence(char operand) {
+	public static int getPrcedence(Object operand) {
 		int precendendeNumber=0;
-		switch(operand) {
+		switch((char)operand) {
 		case '+':precendendeNumber=1;
 			break;
 		case '-':precendendeNumber=1;
@@ -87,12 +28,17 @@ class Precedence {
 }
 public class InfixToPostfix extends StackUsingLinkedList{
 	public static void main(String args[]) {
-		Stack stack=new Stack();
-		Precedence precedenceObject=new Precedence();
+		InfixToPostfix stack=new InfixToPostfix();
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter Infix String");
 		String infixString=sc.next();
 		String postFixString="";
+		List<Object> operandList = new ArrayList<Object>();
+		operandList.add('+');
+		operandList.add('-');
+		operandList.add('*');
+		operandList.add('/');
+		operandList.add('%');
 		int precendencrOfInfixElement,precendencrOfTopElement,infixStingLength=infixString.length();
 		int i=0;
 			while(infixStingLength!=0)
@@ -103,19 +49,19 @@ public class InfixToPostfix extends StackUsingLinkedList{
 				if(currentInfixCharacter=='(')
 					stack.pushAnItem(currentInfixCharacter);
 				else if(currentInfixCharacter==')') {
-					while(stack.top!=null && stack.top.listInformationInChar!='(') {
-						postFixString=postFixString+stack.popAnItem().listInformationInChar;
+					while(!top.nodeInformation.equals((Object)'(')) {
+						postFixString=postFixString+stack.popAnItem();
 					}
 					stack.popAnItem();
 				}
-				else if((currentInfixCharacter=='+'||currentInfixCharacter=='-'||currentInfixCharacter=='*'||currentInfixCharacter=='/'||currentInfixCharacter=='%'||currentInfixCharacter=='('||currentInfixCharacter==')') && stack.top==null) {
+				else if((operandList.contains(currentInfixCharacter)) && top==null) {
 			    	stack.pushAnItem(currentInfixCharacter);
 			    }
-			    else if(currentInfixCharacter=='+'||currentInfixCharacter=='-'||currentInfixCharacter=='*'||currentInfixCharacter=='/'||currentInfixCharacter=='%') {
-			    	precendencrOfInfixElement=precedenceObject.getPrcedence(currentInfixCharacter);
-			    	precendencrOfTopElement=precedenceObject.getPrcedence(stack.top.listInformationInChar);
-			    	while(stack.top!=null && precendencrOfTopElement>=precendencrOfInfixElement && stack.top.listInformationInChar!='(') {
-			    		postFixString=postFixString+stack.popAnItem().listInformationInChar;
+			    else if(operandList.contains(currentInfixCharacter)) {
+			    	precendencrOfInfixElement=Precedence.getPrcedence(currentInfixCharacter);
+			    	precendencrOfTopElement=Precedence.getPrcedence(top.nodeInformation);
+			    	while(top!=null && precendencrOfTopElement>=precendencrOfInfixElement && top.nodeInformation!=(Object)'(') {
+			    		postFixString=postFixString+stack.popAnItem();
 			    	}
 			    	stack.pushAnItem(currentInfixCharacter);
 			    }
@@ -123,10 +69,11 @@ public class InfixToPostfix extends StackUsingLinkedList{
 			    	postFixString=postFixString+currentInfixCharacter;
 			    }
 			}
-			if(stack.top!=null){
-			while(stack.top!=null) 
-				postFixString=postFixString+stack.popAnItem().listInformationInChar;}
+			while(!stack.isEmpty()) 
+				postFixString=postFixString+stack.popAnItem();
 			System.out.println(postFixString);
 		}
 	}
+
+
 
