9a9bc6fa2a2f3f9406455f707f5950debe3a8c24
Avinash-barola <avinash.barola@github.com>
2015-09-04 16:09:35 +0530

    Update CountingSortTest.java

--- DS/DS8/CountingSortTest.java
+++ DS/DS8/CountingSortTest.java
@@ -29,19 +29,19 @@ public class CountingSortTest {
 	public void countingSortTest1() {
 		int list[]={7,6,5,4,3,2,1,0};
 		int expected[]={0,1,2,3,4,5,6,7};
-		assertArrayEquals( expected,CountingSort.countingSort(list,7));
+		assertArrayEquals("Positive test case for Tree Sort", expected,CountingSort.countingSort(list,7));
 	}
 	@Test
 	public void countingSortTest2() {
 		int list[]={0};
 		int expected[]={0};
-		assertArrayEquals( expected,CountingSort.countingSort(list,0));	
+		assertArrayEquals("Positive test case for Tree Sort", expected,CountingSort.countingSort(list,0));	
 	}
 	@Test
 	public void countingSortTest3() {
 		int list[]={-1,-2,1,-4,-22};
 		int expected[]={-1,-2,1,-4,-22};
-		assertArrayEquals( expected,CountingSort.countingSort(list,1));
+		assertArrayEquals( "Negative test case for Tree Sort",expected,CountingSort.countingSort(list,1));
 	}
 
 }
