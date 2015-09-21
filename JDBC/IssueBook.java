03338323a1cdf63a2ffce57468e27abb19696ac0
Avinash-barola <avinash.barola@github.com>
2015-09-21 10:28:50 +0530

    Update IssueBook.java

--- JDBC/IssueBook.java
+++ JDBC/IssueBook.java
@@ -16,41 +16,34 @@ public class IssueBook {
 		String bookName = bufferedReaderObj.readLine(); 
 		System.out.println("Enter Member Name");
 		String memberName = bufferedReaderObj.readLine();
-		IssueBookPOJO issueBookPOJO = new IssueBookPOJO();
 		try {
-			getBooksByAuthorName(bookName,memberName);
-			if(issueBookPOJO.getIssued())
+			BookIssue(bookName,memberName);
+			if(IssueBooks.getIssued())
 				System.out.println("Book Issued Successfully");
-			else if(!(issueBookPOJO.getIssued()))
+			else if(!(IssueBooks.getIssued()))
 				System.out.println("Book Not Issued ");
 		}catch( Exception e){System.out.println("Please Enter correct book name or member name");}
 	}
 	/* execute query using prepared statement*/
 	//method which Return flag to indicate whether the  the book has been issued or not.
-	private static boolean getBooksByAuthorName(String bookName,String memberName) {
+	private static boolean BookIssue(String bookName,String memberName) {
 		//Fetching data using PreparedStatement
 		Connection con = null;
 		PreparedStatement ps = null;
 		ConnectionUtil conUtil = new ConnectionUtil();
 		con = conUtil.getConnection();
 		//Given the name of the book, to be issued by an existing member.
-		String query ="INSERT INTO book_issue(accession_no,member_id) "
-				+ "VALUES( (SELECT b.accession_no "
-				+ "FROM books b,titles t"
-				+ " WHERE t.title_name='"+bookName+"' "
-				+ "AND t.title_id=b.title_id ),"
-				+ "(SELECT member_id FROM members "
-				+ "WHERE member_name='"+memberName+"'));";
+		String query ="INSERT INTO book_issue(accession_no,member_id) VALUES( (SELECT b.accession_no FROM books b,titles t WHERE t.title_name='"+bookName+"'AND t.title_id=b.title_id ),(SELECT member_id FROM members WHERE member_name='"+memberName+"'));";
 		try {
 			ps = con.prepareStatement(query);
 			//set number of rows came from result set
 			int rows = ps.executeUpdate();
 				if(rows>0)
 					//set value of POJO class variable
-					IssueBookPOJO.isIssued=true;
+					IssueBooks.isIssued=true;
 				else
 					//set value of POJO class variable
-					IssueBookPOJO.isIssued=false;
+					IssueBooks.isIssued=false;
 		} catch(Exception e){System.out.println("Please Enter correct book name or member name");} 
 		finally {
 			/* close connection */
@@ -65,6 +58,6 @@ public class IssueBook {
 				e.printStackTrace();
 			}
 		}
-		return IssueBookPOJO.isIssued;
+		return IssueBooks.isIssued;
 	}
 }
